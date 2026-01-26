c52663ac9abdb216aad8afd3630115dd42234f62
Ivan R. Chenoweth <ichenoweth@activa.la>
2026-01-26 00:39:31 +0000

    chore: embed release id v1.5.0-super-warrior in game.js

--- game.js
+++ game.js
@@ -426,7 +426,7 @@ class Game {
         ctx.shadowBlur = 0;
         ctx.font = 'bold 16px "Outfit", sans-serif';
         ctx.fillStyle = '#00ff88';
-        ctx.fillText('v1.4.0-flame-quasar', cx, cy - 150);
+        ctx.fillText('v1.5.0-super-warrior', cx, cy - 150);
 
         // Zoom Controls
         ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
